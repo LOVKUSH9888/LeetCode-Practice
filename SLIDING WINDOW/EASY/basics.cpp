dcbc949234e8273cf42d73f95f1b12830764f490
LOVKUSH <lovkushyadav9888@gmail.com>
2023-03-16 22:59:51 +0530

    Sliding Windows BAsics approach

---
+++ SLIDING WINDOW/EASY/basics.cpp
@@ -0,0 +1,115 @@
+https : // www.codingninjas.com/codestudio/library/sliding-window
+
+// Brute-Force = O(N^2)
+//  C++ program to implement Brute force technique.
+#include <bits/stdc++.h>
+        using namespace std;
+
+// Function to return the maximum sum in a subarray of size 'k'.
+int maximumSum(vector<int> &arr, int n, int k)
+{
+    // If the total number of elements is less than 'k,’ it is invalid.
+    if (n < k)
+    {
+        cout << "Invalid values entered.";
+        return -1;
+    }
+
+    // Initializing the variables.
+    int maxSum = INT_MIN;
+    int curSum;
+
+    // Calculating the sum of first 'k' elements in the array using a nested for loop.
+    for (int i = 0; i <= n - k; i++)
+    {
+        curSum = 0;
+        for (int j = i; j < i + k; j++)
+        {
+            curSum += arr[j];
+        }
+        maxSum = max(maxSum, curSum);
+    }
+    return maxSum;
+}
+int main()
+{
+    vector<int> arr;
+    int n, i, a, k;
+
+    // Taking user input.
+    cout << "Enter the number of elements:\n";
+    cin >> n;
+    cout << "Enter the value of k\n";
+    cin >> k;
+    cout << "Enter the elements\n";
+    for (i = 0; i < n; i++)
+    {
+        cin >> a;
+        arr.push_back(a);
+    }
+
+    // Calling the function.
+    cout << "The maximum sum of a subarray of size " << k << " is " << maximumSum(arr, n, k);
+    return 0;
+};
+
+// ########################################################
+// Sliding Window :-
+//  C++ program to implement Sliding Window Technique.
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to return the maximum sum in a subarray of size 'k'.
+int slidingWindow(vector<int> &arr, int n, int k)
+{
+    // If the total number of elements is less than 'k', it is invalid.
+    if (n < k)
+    {
+        cout << "Invalid values entered.";
+        return -1;
+    }
+
+    // Initializing the variables as 0.
+    int maxSum = 0;
+    int curSum = 0;
+
+    // Calculating the sum of first 'k' elements in the array.
+    for (int i = 0; i < k; i++)
+    {
+        curSum += arr[i];
+    }
+
+    // Initially, the maximum sum is equal to the curSum.
+    maxSum = curSum;
+
+    /*Computing the sum of remaining windows by
+     adding the last element of the current window
+     and removing the first element of the previous window.*/
+    for (int i = k; i < n; i++)
+    {
+        curSum += arr[i] - arr[i - k];
+        maxSum = max(maxSum, curSum);
+    }
+    return maxSum;
+}
+int main()
+{
+    vector<int> arr;
+    int n, i, k, a;
+
+    // Taking user input.
+    cout << "Enter the number of elements:\n";
+    cin >> n;
+    cout << "Enter the value of k:\n";
+    cin >> k;
+    cout << "Enter the elements:\n";
+    for (i = 0; i < n; i++)
+    {
+        cin >> a;
+        arr.push_back(a);
+    }
+
+    // Calling the function.
+    cout << "The maximum sum of a subarray of size " << k << " is " << slidingWindow(arr, n, k);
+    return 0;
+}
